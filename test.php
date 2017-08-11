6a2190298d482c70b1a7afbbdccb3686906beacf
toavina <toavinar@outlook.com>
2017-08-11 16:04:34 +0300

    tesst

--- test.php
+++ test.php
@@ -1,9 +1,16 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-try {
-    $instance = \ExponentPhpSDK\Expo::normalSetup();
-    echo 'Succeeded! We have created an Expo instance successfully';
-} catch (Exception $e) {
-    echo 'Test Failed';
-}
+  require_once __DIR__.'/vendor/autoload.php';
+  
+  $interestDetails = ['unique identifier', 'ExpoPushToken[unique]'];
+  
+  // You can quickly bootup an expo instance
+  $expo = \ExponentPhpSDK\Expo::normalSetup();
+  
+  // Subscribe the recipient to the server
+  $expo->subscribe($interestDetails[0], $interestDetails[1]);
+  
+  // Build the notification data
+  $notification = ['body' => 'Hello World!];
+  
+  // Notify an interest with a notification
+  $expo->notify($interesDetails[0], $notification);
