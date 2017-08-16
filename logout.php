41e105b6230e9f05d699a37605f4bc79a335166b
toavina <toavinar@outlook.com>
2017-08-16 16:32:10 +0300

    test

--- logout.php
+++ logout.php
@@ -3,19 +3,7 @@
     if(isset($_POST['username'])){
       require_once __DIR__.'/vendor/autoload.php';
       $username = [$_POST['username']];
-      
-      // You can quickly bootup an expo instance
       $expo = \ExponentPhpSDK\Expo::normalSetup();
-      
-      
-      $message = 'Bonjour '.$interestDetails[0]." ce mobile n'est plus synchronisé avec votre compte et ne recevra plus de notification";
-      // Build the notification data
-      $notification = ['body' => $message];
-      
-      // Notify an interest with a notification
-      $expo->notify($interestDetails[0], $notification);
-
-      // Subscribe the recipient to the server
       $expo->unsubscribe($username);
       echo(json_encode($_POST));
     }else{
