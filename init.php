0b417c01896fc4140f496c9515adad30aaa4c818
toavina <toavinar@outlook.com>
2017-08-16 12:44:13 +0300

    test

--- init.php
+++ init.php
@@ -1,28 +1,22 @@
   <?php
     header("Access-Control-Allow-Origin: *");
 
-    var_dump('eto aza n ol tzitra kou');
-     include_once 'InitUser.php';
-     test("Azerty");
-      // $rest = new InitUser();
-    // include_once '/initUser.php';
-    // $rest = new initUser();
-    // $rest->goInit();
-  //   // You can quickly bootup an expo instance
-  //     $expo = \ExponentPhpSDK\Expo::normalSetup();
+     
+    // You can quickly bootup an expo instance
+      $expo = \ExponentPhpSDK\Expo::normalSetup();
 
-    
-  //     // $interest = $_POST['username'];
-  //     // $token = $_POST['token'];
-  //     // $interestDetails = [$interest, $token]      
-  //     // $expo->subscribe($interest, $token);
-  //     // $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
-  //     //           une notification vous sera envoyée à chaque reception d'argent";
-  //     // // Build the notification data
-  //     // $notification = ['body' => $message];
       
-  //     // // Notify an interest with a notification
-  //     // $expo->notify($interest, $notification);
+      $interest = (isset($_POST['username'])) ? $_POST['username'] : "toavina";
+      $token = (isset($_POST['token'])) ? $_POST['token'] : "ExponentPushToken[5AQ172ILixXkR3_9oM9uTh";
+      $interestDetails = [$interest, $token];      
+      $expo->subscribe($interest, $token);
+      $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
+                une notification vous sera envoyée à chaque reception d'argent";
+      // Build the notification data
+      $notification = ['body' => $message];
+      
+      // Notify an interest with a notification
+      $expo->notify($interest, $notification);
 
         
         
