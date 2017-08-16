b6e6ca02201b3f5e1d132dd87eb84902656fb03f
toavina <toavinar@outlook.com>
2017-08-16 11:28:12 +0300

    test

--- init.php
+++ init.php
@@ -1,34 +1,31 @@
   <?php
+  
+  header("Access-Control-Allow-Origin: *");
   require_once __DIR__.'/vendor/autoload.php';
   require_once '/SimpleRestHTTP.php';
-
-    header("Access-Control-Allow-Origin: *");
     // You can quickly bootup an expo instance
       $expo = \ExponentPhpSDK\Expo::normalSetup();
 
-    if(isset($_POST)){
-      $interest = $_POST['username'];
-      $token = $_POST['token'];
-      $interestDetails = [$interest, $token];
+    
+      // $interest = $_POST['username'];
+      // $token = $_POST['token'];
+      // $interestDetails = [$interest, $token]      
+      // $expo->subscribe($interest, $token);
+      // $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
+      //           une notification vous sera envoyée à chaque reception d'argent";
+      // // Build the notification data
+      // $notification = ['body' => $message];
       
-      $expo->subscribe($interestDetails[0], $interestDetails[1]);
-      $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
-                une notification vous sera envoyée à chaque reception d'argent";
-      // Build the notification data
-      $notification = ['body' => $message];
-      
-      // Notify an interest with a notification
-      $expo->notify($interestDetails[0], $notification);
+      // // Notify an interest with a notification
+      // $expo->notify($interest, $notification);
 
         $statusCode = 200;
-        $response = array('accountId'=>5);
+        $response = array('test'=>'123ze');
 
         $requestContentType = $_SERVER['HTTP_ACCEPT'];
         $this ->setHttpHeaders($requestContentType, $statusCode);
 
         $response = $this->encodeJson($response);
         echo $response;
-
-    }
 
     
