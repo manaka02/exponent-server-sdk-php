c7c8e2d1151f770d43fa04c4b0408c8e67d05bfc
toavina <toavinar@outlook.com>
2017-08-16 11:30:35 +0300

    test

--- init.php
+++ init.php
@@ -1,31 +1,31 @@
   <?php
   
-  header("Access-Control-Allow-Origin: *");
-  require_once __DIR__.'/vendor/autoload.php';
-  require_once '/SimpleRestHTTP.php';
-    // You can quickly bootup an expo instance
-      $expo = \ExponentPhpSDK\Expo::normalSetup();
+  // header("Access-Control-Allow-Origin: *");
+  // require_once __DIR__.'/vendor/autoload.php';
+  // require_once '/SimpleRestHTTP.php';
+  //   // You can quickly bootup an expo instance
+  //     $expo = \ExponentPhpSDK\Expo::normalSetup();
 
     
-      // $interest = $_POST['username'];
-      // $token = $_POST['token'];
-      // $interestDetails = [$interest, $token]      
-      // $expo->subscribe($interest, $token);
-      // $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
-      //           une notification vous sera envoyée à chaque reception d'argent";
-      // // Build the notification data
-      // $notification = ['body' => $message];
+  //     // $interest = $_POST['username'];
+  //     // $token = $_POST['token'];
+  //     // $interestDetails = [$interest, $token]      
+  //     // $expo->subscribe($interest, $token);
+  //     // $message = "Bienvenue sur l'application AriaryPro ".$interest." \n 
+  //     //           une notification vous sera envoyée à chaque reception d'argent";
+  //     // // Build the notification data
+  //     // $notification = ['body' => $message];
       
-      // // Notify an interest with a notification
-      // $expo->notify($interest, $notification);
+  //     // // Notify an interest with a notification
+  //     // $expo->notify($interest, $notification);
 
-        $statusCode = 200;
-        $response = array('test'=>'123ze');
+  //       $statusCode = 200;
+  //       $response = array('test'=>'123ze');
 
-        $requestContentType = $_SERVER['HTTP_ACCEPT'];
-        $this ->setHttpHeaders($requestContentType, $statusCode);
+  //       $requestContentType = $_SERVER['HTTP_ACCEPT'];
+  //       $this ->setHttpHeaders($requestContentType, $statusCode);
 
-        $response = $this->encodeJson($response);
-        echo $response;
+  //       $response = $this->encodeJson($response);
+        echo "eo ara oe";
 
     
