8dbbf3f64cc67b456ed78ba93fc86e0b902cd76c
toavina <toavinar@outlook.com>
2017-08-16 16:41:33 +0300

    test

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@
       $username = [$_POST['username']];
       $expo = \ExponentPhpSDK\Expo::normalSetup();
       $expo->unsubscribe($username);
-      echo(json_encode($_POST));
+      echo(json_encode($_POST.'eto le post'));
     }else{
       echo('tsis inin le POST ah');
     }
