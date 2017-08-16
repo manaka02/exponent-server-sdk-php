5ae9f8bdb9d8d8fbb05ff88856df4e5910993eac
toavina <toavinar@outlook.com>
2017-08-16 09:48:58 +0300

    test

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php
+  <?php
   require_once __DIR__.'/vendor/autoload.php';
   
-  $interestDetails = ['unique identifier', 'ExpoPushToken[ejp4vuF52ZTNBKBvw53vys]'];
-  
-  // You can quickly bootup an expo instance
-  $expo = \ExponentPhpSDK\Expo::normalSetup();
-  
-  // Build the notification data
-  $notification = ['body' => 'Hello World!'];
-  
-  // Notify an interest with a notification
-  $expo->notify($interesDetails[0], $notification);
+    $interestDetails = ['toavina', 'ExponentPushToken[5AQ172ILixXkR3_9oM9uTh]'];
+    
+    // You can quickly bootup an expo instance
+    $expo = \ExponentPhpSDK\Expo::normalSetup();
+    
+    // Subscribe the recipient to the server
+    $expo->subscribe($interestDetails[0], $interestDetails[1]);
+    
+    // Build the notification data
+    $notification = ['body' => 'Eo ara oe!'];
+    
+    // Notify an interest with a notification
+    $expo->notify($interestDetails[0], $notification);
