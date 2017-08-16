a19263cec353191efcd5f243b862062638f8f987
toavina <toavinar@outlook.com>
2017-08-16 14:16:10 +0300

    test

--- init.php
+++ init.php
@@ -1,10 +1,8 @@
   <?php
-    header("Access-Control-Allow-Origin: *");
 
-    if(isset($_POST)){
-      var_dump($_POST);
+    if(isset($_POST['token'])){
       require_once __DIR__.'/vendor/autoload.php';
-      $interestDetails = ['toavina', 'ExponentPushToken[5AQ172ILixXkR3_9oM9uTh]'];
+      $interestDetails = [$_POST['username'], $_POST['token']];
       
       // You can quickly bootup an expo instance
       $expo = \ExponentPhpSDK\Expo::normalSetup();
@@ -13,7 +11,8 @@
       $expo->subscribe($interestDetails[0], $interestDetails[1]);
       
       // Build the notification data
-      $notification = ['body' => 'Hello World!'];
+      $notification = ['body' => 'Bonjour '.$interestDetails[0]."
+       on vous enverra désormais une notification sur ce mobile à chaque reception d'argent"];
       
       // Notify an interest with a notification
       $expo->notify($interestDetails[0], $notification);
