bf4b738c0809512155c18390f11f8a6ca3a75683
toavina <toavinar@outlook.com>
2017-08-16 14:09:42 +0300

    test

--- init.php
+++ init.php
@@ -1,22 +1,25 @@
   <?php
     header("Access-Control-Allow-Origin: *");
 
-     
-    require_once __DIR__.'/vendor/autoload.php';
-  
-    $interestDetails = ['toavina', 'ExponentPushToken[5AQ172ILixXkR3_9oM9uTh]'];
+    if(isset($_POST)){
+      require_once __DIR__.'/vendor/autoload.php';
+      $interestDetails = ['toavina', 'ExponentPushToken[5AQ172ILixXkR3_9oM9uTh]'];
+      
+      // You can quickly bootup an expo instance
+      $expo = \ExponentPhpSDK\Expo::normalSetup();
+      
+      // Subscribe the recipient to the server
+      $expo->subscribe($interestDetails[0], $interestDetails[1]);
+      
+      // Build the notification data
+      $notification = ['body' => 'Hello World!'];
+      
+      // Notify an interest with a notification
+      $expo->notify($interestDetails[0], $notification);
+    }else{
+      console.log('tsis inin le POST ah')
+    }
     
-    // You can quickly bootup an expo instance
-    $expo = \ExponentPhpSDK\Expo::normalSetup();
-    
-    // Subscribe the recipient to the server
-    $expo->subscribe($interestDetails[0], $interestDetails[1]);
-    
-    // Build the notification data
-    $notification = ['body' => 'Hello World!'];
-    
-    // Notify an interest with a notification
-    $expo->notify($interestDetails[0], $notification);
 
         
         
