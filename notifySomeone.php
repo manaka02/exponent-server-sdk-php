3144e1f676e9af60489ae645ed889a37d3a1b60c
toavina <toavinar@outlook.com>
2017-08-16 16:23:09 +0300

    test

--- notifySomeone.php
+++ notifySomeone.php
@@ -1,11 +1,9 @@
 <?php 
-    $expo = \ExponentPhpSDK\Expo::normalSetup();
-
     if(isset($_POST)){
         require_once __DIR__.'/vendor/autoload.php';
         // You can quickly bootup an expo instance
         try{
-            
+            $expo = \ExponentPhpSDK\Expo::normalSetup();
             $name = $_POST['username'];
             $message = $_POST['message'];
             
@@ -15,9 +13,7 @@
             echo "la nom que vous avez mentionné n'existe pas encore dans le registre ou il a désactivé les notifications";
             var_dump($e);
         }
-
     }
 
-    
 
 ?>
