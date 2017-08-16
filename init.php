08e3c844130efb2d90d49d245437ad90d384f40d
toavina <toavinar@outlook.com>
2017-08-16 17:11:37 +0300

    test

--- init.php
+++ init.php
@@ -6,11 +6,21 @@
       
       // You can quickly bootup an expo instance
       $expo = \ExponentPhpSDK\Expo::normalSetup();
+      $message = '';
+      // Subscribe the recipient to the server
+      try{
+        $listName = [$interestDetails[0]];
+        $tokens = $expo->getMembers($listName);
+        $message = 'rebonjour '.$interestDetails[0];
+
+      }catch(Exception $e){
         $message = 'Bonjour '.$interestDetails[0]."\n on vous enverra désormais \n une notification sur ce mobile \n à chaque récéption d'argent";
         $expo->subscribe($interestDetails[0], $interestDetails[1]);
-        $notification = ['body' => $message];
-        $expo->notify($interestDetails[0], $notification);
-        echo json_encode($notification);
+        echo(json_encode($e));
+      }
+      $notification = ['body' => $message];
+      $expo->notify($interestDetails[0], $notification);
+        echo json_encode($_POST);
     }else{
       echo('tsis inin le POST ah');
     }
