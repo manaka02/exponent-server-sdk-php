4524f1dd0913bc2a70d1f2d68d25f862c4eb9944
toavina <toavinar@outlook.com>
2017-08-16 10:17:16 +0300

    test

--- notifySomeone.php
+++ notifySomeone.php
@@ -3,18 +3,16 @@
         require_once __DIR__.'/vendor/autoload.php';
         // You can quickly bootup an expo instance
         var_dump($_POST);
-        // try{
-        //     $expo = \ExponentPhpSDK\Expo::normalSetup();
-        //     $name = $_POST['username'];
-        //     $message = $_POST['message'];
-        //     $notification = ['body' => $message];
-        //     $name=trim($name);
-        //     $expo->notify($name, $notification);
-        // } catch(Exception $e){
-        //     var_dump($e)
-        // }
+        try{
+            $expo = \ExponentPhpSDK\Expo::normalSetup();
+            $name = $_POST['username'];
+            $message = $_POST['message'];
+            $notification = ['body' => $message];
+            $expo->notify($name, $notification);
+        } catch(Exception $e){
+            var_dump($e)
+        }
     }
-    echo('eto v')
 
 
 ?>
