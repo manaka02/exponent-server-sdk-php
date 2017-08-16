518df015742995e8df727cc49419dc1ffcd57efe
toavina <toavinar@outlook.com>
2017-08-16 11:38:09 +0300

    test

--- init.php
+++ init.php
@@ -3,8 +3,16 @@
     var_dump($_POST);
     echo "eo ara oe";
   // header("Access-Control-Allow-Origin: *");
-  require_once __DIR__.'/vendor/autoload.php';
-  include_once '/SimpleRestHTTP.php';
+    require_once __DIR__.'/vendor/autoload.php';
+    include_once '/SimpleRestHTTP.php';
+    $statusCode = 200;
+    $response = $_POST;
+
+    $requestContentType = $_SERVER['HTTP_ACCEPT'];
+    $this ->setHttpHeaders($requestContentType, $statusCode);
+
+    $response = $this->encodeJson($response);
+    echo($response)
   //   // You can quickly bootup an expo instance
   //     $expo = \ExponentPhpSDK\Expo::normalSetup();
 
@@ -21,13 +29,7 @@
   //     // // Notify an interest with a notification
   //     // $expo->notify($interest, $notification);
 
-        $statusCode = 200;
-        $response = $_POST;
-
-        $requestContentType = $_SERVER['HTTP_ACCEPT'];
-        $this ->setHttpHeaders($requestContentType, $statusCode);
-
-        $response = $this->encodeJson($response);
+        
         
 
     
